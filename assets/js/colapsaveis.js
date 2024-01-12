0ff211422945b49fe411ea9f7c62a839dfb490d2
DChrispim <brenoadsdc@gmail.com>
2024-01-12 18:14:07 -0300

    Adição de comentários e modificação no carregamento do script colapsaveis.js

--- assets/js/colapsaveis.js
+++ assets/js/colapsaveis.js
@@ -1,30 +1,23 @@
-// Função para adicionar eventos nos elementos da classe colapsáveis
-function elementosColapsaveis () {
-
+/**
+ * Função que adiciona EventListener nos elementos da classe colapsaveis
+ */
+function adicionaEventoColapsaveis () {
     // Definições das variáveis
-    var arrayElementos = document.getElementsByClassName("colapsaveis");
-    var i;
+    let arrayElementos = document.getElementsByClassName("colapsaveis");
 
     // Loop sob todos os elementos da classe colapsáveis
-    for (i = 0; i < arrayElementos.length; i++) {
-
-        // Adição de evento click
+    for (let i = 0; i < arrayElementos.length; i++) {
         arrayElementos[i].addEventListener("click", function() {
-
-            // Ativa o modo ativo do elemento
             this.classList.toggle("ativo");
-
-            // Pega o elemento filho (conteúdo a ser exibido)
-            var content = this.nextElementSibling;
-
-            // Modifica maxHeight para mostrar o conteúdo
+            let content = this.nextElementSibling;
             if (content.style.maxHeight){
-                // Fechar
                 content.style.maxHeight = null;
             } else {
-                // Abrir
                 content.style.maxHeight = content.scrollHeight + "px";
             }
         });
     }
 }
+
+
+adicionaEventoColapsaveis()
