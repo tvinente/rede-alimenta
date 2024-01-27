3a3417f6f6b1b221f79d0bcbd9c5ca918411b669
Ciane <100417980+ciane-pereira@users.noreply.github.com>
2024-01-27 15:44:22 -0300

    estilização slide e ranking

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-/***slider***/
+/***slider
 let count = 1;
 document.getElementById("radio1").checked = true;
 
@@ -13,4 +13,19 @@ function nextImage(){
     }
 
     document.getElementById("radio"+count).checked = true
+}***/
+
+let slideIndex = 1;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 8000); // Change image every 2 seconds
 }
