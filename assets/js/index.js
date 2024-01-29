bd3508416dfcb251e86809a5642dba7caf869415
Ciane <100417980+ciane-pereira@users.noreply.github.com>
2024-01-29 12:42:06 -0300

    ajustes na formatação

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,28 @@
-/***slider
-let count = 1;
-document.getElementById("radio1").checked = true;
-
-setInterval(function(){
-    nextImage();
-}, 7000)
-
-function nextImage(){
-    count++;
-    if(count>4){
-        count = 1;
-    }
-
-    document.getElementById("radio"+count).checked = true
-}***/
+/***slider*****/
 
 let slideIndex = 1;
-showSlides();
+showSlides(slideIndex);
 
-function showSlides() {
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  let botoes = document.getElementsByClassName("botao");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < botoes.length; i++) {
+    botoes[i].className = botoes[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 8000); // Change image every 2 seconds
+  botoes[slideIndex-1].className += " active";
 }
